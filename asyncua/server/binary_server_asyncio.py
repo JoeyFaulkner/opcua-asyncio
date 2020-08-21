36b4680e5eb975f20ceb81a206be06819847ddc4
Joey Faulkner <joeymfaulkner@gmail.com>
2020-08-21 16:52:42 +0200

    change warnings to infos

--- asyncua/server/binary_server_asyncio.py
+++ asyncua/server/binary_server_asyncio.py
@@ -139,9 +139,9 @@ class BinaryServer:
         self.logger.info('Listening on %s:%s', self.hostname, self.port)
 
     def _clear_cache(self):
-        self.logger.warning(f'protocol tasks being cleared, before:{len(self.protocol_tasks)}')
+        logger.info(f'protocol tasks being cleared, before:{len(self.protocol_tasks)}')
         self.protocol_tasks = [task for task in self.protocol_tasks if not (task.done() or task.cancelled())]
-        self.logger.warning(f'after {len(self.protocol_tasks)}')
+        logger.info(f'after {len(self.protocol_tasks)}')
 
     async def stop(self):
         self.logger.info('Closing asyncio socket server')
